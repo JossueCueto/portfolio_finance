5c2af895706ea98181baeb85a53d82d1d0161641
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-25 21:04:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st 
 import numpy as np 
 import pandas as pd
-from PIL import Image
+#from PIL import Image
 #from streamlit_timeline import timeline
-import plotly.express as px
-import plotly.figure_factory as ff
+#import plotly.express as px
+#import plotly.figure_factory as ff
 import requests
 import re
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 import io
 import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
