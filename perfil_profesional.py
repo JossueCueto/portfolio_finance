3c37b016b2c90670e05f6d6802d6916870335b06
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-27 17:59:49 -0500

    Update perfil_profesional.py

--- perfil_profesional.py
+++ perfil_profesional.py
@@ -9,8 +9,14 @@ def run_perfil_profesional():
   st.write('Cursos relevantes')
   st.subheader('Go to Market - NetValU')
   st.write('Seleccionado para un programa altamente competitivo de finanzas corporativas, inversiones, riesgos y empleabilidad, seleccionado entre más de 170 candidatos a nivel nacional para ser uno de los 20 participantes')
+  # Your markdown content before the download button
   st.markdown("""
   - **Propuesta de Portafolio de inversión de renta fija**: Elaboración de un portafolio de inversión compuesto por 100% bonos corporativos en base a un análisis macroeconómico, sectorial y de ratios financieros de las empresas.
+  """)
+  # The download button should be a standalone call to st.download_button()
+  st.download_button('Download Resume', 'path_to_resume.pdf', 'Tu Resumen.pdf')
+  # Continue with your markdown content after the download button
+  st.markdown("""
   - **Medición de Riesgos de Portafolio de Inversión**: Análisis del riesgo de mercado y su impacto en el portafolio, considerando variables macroeconómicas y sensibilidad a factores de riesgo.
   - **Evaluación del Riesgo Crediticio**: Evaluación de la calidad crediticia para la compra de un bono corporativo de la empresa H2Olmos usando métricas de riesgo de crédito.
   - **Budget de Flujo de Caja y P&L**: Estimación de los resultados financieros de la compañía Netflix en los últimos 2 trimestres del 2023.
