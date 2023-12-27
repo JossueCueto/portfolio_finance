7920bf098b42fdf128baead0cbf0ad901bc6d6b2
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-27 13:01:02 -0500

    Update perfil_profesional.py

--- perfil_profesional.py
+++ perfil_profesional.py
@@ -4,7 +4,9 @@ def run_perfil_profesional():
   st.subheader('Resumen Profesional')
   st.write('Aquí puedes escribir un resumen de tu introducción a la ciencia de datos o cualquier otro contenido.')
   st.header('Educación y Formación Académica')
-  st.subheader('Universidad Nacional Jorge Basadre Grohmann')
+  st.subheader('Ciencias Administrativas - UNJBG')
+  st.write('Bachiller en Ciencias Administrativas con formación en ')
+  st.write('Cursos relevantes')
   st.subheader('Go to Market - NetValU')
   st.write('Seleccionado para un programa altamente competitivo de finanzas corporativas, inversiones, riesgos y empleabilidad, seleccionado entre más de 170 candidatos a nivel nacional para ser uno de los 20 participantes')
   st.markdown("""
@@ -13,8 +15,11 @@ def run_perfil_profesional():
   - **Evaluación del Riesgo Crediticio**: Evaluación de la calidad crediticia para la compra de un bono corporativo de la empresa H2Olmos usando métricas de riesgo de crédito.
   - **Budget de Flujo de Caja y P&L**: Estimación de los resultados financieros de la compañía Netflix en los últimos 2 trimestres del 2023.
   """)
-  st.subheader('Objetivos Profesionales')
-  st.write('Aquí puedes escribir un resumen de tu introducción a la ciencia de datos o cualquier otro contenido.')
+  st.subheader('Objetivos Profesionales 2024')
+  st.subheader('CEFA - BCRP')
+  st.write('Al haber sido seleccionado para participar en el Curso de Extensión de Finanzas Avanzadas (CEFA) organizado por el BCRP a realizarse de Enero a Marzo del 2024, me plantee como objetivo acabar entre los primeros puestos a fin de demostrar mi capacidad y conocimiento en el área financiera')
+  st.subheader('CFA')
+  st.write('Para el año 2024 tengo planteado aprobar el primer nivel del CFA con el fin de acreditar y consolidar mi conocimiento')
   st.subheader('Desarrollo Personal y Profesional')
   st.write('Aquí puedes escribir un resumen de tu introducción a la ciencia de datos o cualquier otro contenido.')
   st.subheader('Experiencia adicional')
