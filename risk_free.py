fe727cbf0b52153c3ba51ed19be556ce9fffa40c
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-26 19:36:44 -0500

    Update risk_free.py

--- risk_free.py
+++ risk_free.py
@@ -4,11 +4,5 @@ import pandas_datareader.data as pdr
 def risk_free_function(start=dt.date.today() - dt.timedelta(days=30),end=dt.date.today()):
     risk_free_data=pdr.get_data_yahoo('^TNX',start,end)['Adj Close']
     risk_free_rate = float(risk_free_data.iloc[-1])
+    risk_free_rate=risk_free_rate/100
     return print(risk_free_rate)
-
-risk_free_function()
-    if last_valid_risk_free_rate is not None:
-        last_valid_risk_free_rate=last_valid_risk_free_rate
-    else:
-        last_valid_risk_free_rate="No se pudo encontrar un valor válido en los últimos intentos."
-    return last_valid_risk_free_rate/100
