4c3126592a2930a2f46912b773e8fbbf5d295acd
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-27 11:57:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@ import streamlit as st
 from portfolio_simulation import run_simulation
 from html_module import generate_html
 
-
-# Encabezado de la aplicación
-st.title('Mi Aplicación de Data Science')
-
 #Guardar contenido HTML
 html_content = generate_html()
 
@@ -16,23 +12,25 @@ with st.sidebar:
     st.write(" ")
     # Puedes usar st.markdown para un control más fino del formato de texto, si es necesario
     st.markdown('Bachiller en Ciencias Administrativas con dominio intermedio de inglés, habilidades analíticas y comunicativas, enfocado en el mercado de capitales y gestión de portafolios.')
-# Agrega más información debajo de las columnas anidadas en la barra lateral si es necesario
-st.sidebar.write("Wish to connect?")
-st.sidebar.write("jossue.cueto@gmail.com")  # Asegúrate de reemplazar esto con tu información real
-st.sidebar.write("www.linkedin.com/in/jossue-cueto/")  # Asegúrate de reemplazar esto con tu información real
-st.sidebar.download_button('Download Resume', 'path_to_resume.pdf', 'Tu Resumen.pdf')
+    st.write("Wish to connect?")
+    st.write("jossue.cueto@gmail.com")  # Asegúrate de reemplazar esto con tu información real
+    st.write("www.linkedin.com/in/jossue-cueto/")  # Asegúrate de reemplazar esto con tu información real
+    st.download_button('Download Resume', 'path_to_resume.pdf', 'Tu Resumen.pdf')
 
 # Menú desplegable en la barra lateral
 menu = st.sidebar.selectbox('Menu', ['Sobre mi', 'Trabajos'])
 
 # Contenido principal de la aplicación que cambia basado en la selección del menú
 if menu == 'Sobre mi':
+    # Encabezado de la aplicación
+    st.title('Mi Aplicación de Data Science')
     # Contenido de 'Sobre mi'
     st.header('Snapshot de la Carrera')
     st.subheader('Mi Introducción a Data Science')
     st.write('Aquí puedes escribir un resumen de tu introducción a la ciencia de datos o cualquier otro contenido.')
     # Puedes agregar más secciones aquí relacionadas con 'Sobre mi'
 elif menu == 'Trabajos':
+    st.title('Optimización de portafolio')
     run_simulation()
 
 # Para ejecutar la aplicación, guarda el código en un archivo .py y ejecútalo con el comando:
