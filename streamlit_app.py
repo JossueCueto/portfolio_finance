5d0a2e6c4443fb8dd1f74a5388037a56646cf2b9
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-26 21:27:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,34 @@
+import streamlit as st
+from portfolio_simulation import run_simulation
+
 # Encabezado de la aplicación
 st.title('Mi Aplicación de Data Science')
 
+import streamlit as st
+
+# Utilizando st.columns dentro de la barra lateral para crear un diseño de columna anidada
 with st.sidebar:
-    # Añade un espaciador en la parte superior si es necesario
-    st.empty()
-
-    # Define las columnas con una proporción que prefieras
-    col1, col2 = st.columns([1, 4])
-
+    # Usamos un espaciador antes de las columnas para añadir algo de espacio si es necesario
+    st.write(" ")  # Añade espacios en blanco para más espacio si es necesario
+    col1, col2 = st.columns([1, 3])
     with col1:
-        # Añade la imagen
         st.image('foto.jpg', width=80)  # Ajusta el tamaño de la imagen como sea necesario
-
     with col2:
-        # Añade un espaciador horizontal antes del texto para incrementar el espacio
-        st.write(" ")  # Incrementa el número de espacios si es necesario
-
-        # Añade el texto
-        st.write('Jossue Cueto')
-        st.write('Bachiller en Ciencias Administrativas, interesado en el mercado de capitales')
-
-    # Añade un espaciador en la parte inferior si es necesario
-    st.empty()
+        # Puedes usar st.markdown para un control más fino del formato de texto, si es necesario
+        st.markdown('**Jossue Cueto**')
+        st.markdown('Bachiller en Ciencias Administrativas, interesado en el mercado de capitales')
+        # Añade espacios en blanco después del texto si es necesario
+        st.write(" ")
 
 # Agrega más información debajo de las columnas anidadas en la barra lateral si es necesario
 st.sidebar.write("Wish to connect?")
 st.sidebar.write("jossue.cueto@gmail.com")  # Asegúrate de reemplazar esto con tu información real
 st.sidebar.write("www.linkedin.com/in/jossue-cueto/")  # Asegúrate de reemplazar esto con tu información real
 st.sidebar.download_button('Download Resume', 'path_to_resume.pdf', 'Tu Resumen.pdf')
+
 # Menú desplegable en la barra lateral
 menu = st.sidebar.selectbox('Menu', ['Sobre mi', 'Trabajos'])
+
 # Contenido principal de la aplicación que cambia basado en la selección del menú
 if menu == 'Sobre mi':
     # Contenido de 'Sobre mi'
@@ -39,7 +38,10 @@ if menu == 'Sobre mi':
     # Puedes agregar más secciones aquí relacionadas con 'Sobre mi'
 elif menu == 'Trabajos':
     run_simulation()
+
 # Para ejecutar la aplicación, guarda el código en un archivo .py y ejecútalo con el comando:
 # streamlit run tu_archivo.py
+
+# Asegúrate de actualizar 'foto.jpg' y 'path_to_resume.pdf' con las rutas reales a tus archivos.
 
 
