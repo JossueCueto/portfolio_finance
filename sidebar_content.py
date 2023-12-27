d432dafc1cb49c5bb2e8f20aa1a07063c41c9f9e
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-26 21:07:44 -0500

    Update sidebar_content.py

--- sidebar_content.py
+++ sidebar_content.py
@@ -5,12 +5,16 @@ def load_css():
     sidebar_css = """
     <style>
     .image-container {
-        margin-right: 20px;  /* Aumenta el espacio entre la imagen y el texto */
+        padding-right: 20px;  /* Aumenta el espacio entre la imagen y el texto */
     }
     .text-container {
         display: flex;
         flex-direction: column;
         justify-content: center;
+    }
+    /* Ajusta el ancho de la imagen para evitar que ocupe demasiado espacio */
+    .stImage {
+        width: auto !important;
     }
     </style>
     """
@@ -19,7 +23,7 @@ def load_css():
 def display_sidebar_content():
     with st.sidebar:
         st.markdown('<div class="image-container">', unsafe_allow_html=True)
-        st.image('foto.jpg', width=80)
+        st.image('foto.jpg', width=80)  # Ajusta el ancho de la imagen según sea necesario
         st.markdown('</div>', unsafe_allow_html=True)
         
         st.markdown('<div class="text-container">', unsafe_allow_html=True)
