4fcea941fd6f69feb53f5956dfcfbce43808202d
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-27 12:25:36 -0500

    Update Perfil_profesional.py

--- Perfil_profesional.py
+++ Perfil_profesional.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+def run_perfil_profesional:
+  st.subheader('Resumen Profesional')
+  st.write('Aquí puedes escribir un resumen de tu introducción a la ciencia de datos o cualquier otro contenido.')
+  st.header('Educación y Formación Académica')
+  st.subheader('NetValU')
+  st.markdown("""
+  - **Propuesta de Portafolio de inversión de renta fija**: Elaboración de un portafolio de inversión compuesto por 100% bonos corporativos en base a un análisis macroeconómico, sectorial y de ratios financieros de las empresas.
+  - **Medición de Riesgos de Portafolio de Inversión**: Análisis del riesgo de mercado y su impacto en el portafolio, considerando variables macroeconómicas y sensibilidad a factores de riesgo.
+  - **Evaluación del Riesgo Crediticio**: Evaluación de la calidad crediticia para la compra de un bono corporativo de la empresa H2Olmos usando métricas de riesgo de crédito.
+  - **Budget de Flujo de Caja y P&L**: Estimación de los resultados financieros de la compañía Netflix en los últimos 2 trimestres del 2023.
+  """)
+  st.subheader('Objetivos Profesionales')
+  st.write('Aquí puedes escribir un resumen de tu introducción a la ciencia de datos o cualquier otro contenido.')
+  st.subheader('Desarrollo Personal y Profesional')
+  st.write('Aquí puedes escribir un resumen de tu introducción a la ciencia de datos o cualquier otro contenido.')
+  st.subheader('Experiencia adicional')
+  st.write('Aquí puedes escribir un resumen de tu introducción a la ciencia de datos o cualquier otro contenido.')
