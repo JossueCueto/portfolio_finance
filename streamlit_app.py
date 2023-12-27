1d90427b7a741f4805a2d064a148db15a3f5bec3
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-27 17:43:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,37 +3,31 @@ from portfolio_simulation import run_simulation
 from html_module import generate_html
 from perfil_profesional import run_perfil_profesional
 
-#Guardar contenido HTML
+# Guardar contenido HTML
 html_content = generate_html()
 
 with st.sidebar:
     st.markdown(html_content, unsafe_allow_html=True)
     st.write(" ")
-    # Puedes usar st.markdown para un control más fino del formato de texto, si es necesario
     st.write("Wish to connect?")
     st.write("jossue.cueto@gmail.com")  # Asegúrate de reemplazar esto con tu información real
     st.write("www.linkedin.com/in/jossue-cueto/")  # Asegúrate de reemplazar esto con tu información real
     st.download_button('Download Resume', 'path_to_resume.pdf', 'Tu Resumen.pdf')
 
-# Menú desplegable en la barra lateral
-menu = st.sidebar.selectbox('Menu', ['Sobre mi', 'Trabajos'])
+    # Menú desplegable en la barra lateral
+    menu = st.sidebar.selectbox('Menu', ['Sobre mi', 'Trabajos'])
+
+# Condición para mostrar el texto adicional en la barra lateral, antes del menú
 if menu != 'Sobre mi':
     with st.sidebar:
         st.markdown('Bachiller en Ciencias Administrativas con dominio intermedio de inglés, habilidades analíticas y comunicativas, enfocado en el mercado de capitales y gestión de portafolios.')
 
 # Contenido principal de la aplicación que cambia basado en la selección del menú
 if menu == 'Sobre mi':
-    # Encabezado de la aplicación
     st.title('Perfil Profesional')
-    # Contenido de 'Sobre mi'
     run_perfil_profesional()
 elif menu == 'Trabajos':
     st.title('Optimización de portafolio')
     run_simulation()
-
-# Para ejecutar la aplicación, guarda el código en un archivo .py y ejecútalo con el comando:
-# streamlit run tu_archivo.py
-
-# Asegúrate de actualizar 'foto.jpg' y 'path_to_resume.pdf' con las rutas reales a tus archivos.
 
 
