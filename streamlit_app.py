f6b2c563e615f62705ad6c1b2bbe67b74a980e64
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-28 10:54:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from portfolio_simulation import run_portfolio_simulation
 from html_module import generate_html
 from perfil_profesional import run_perfil_profesional
+import requests
+from io import BytesIO
 
 # Guardar contenido HTML
 html_content = generate_html()
@@ -18,6 +20,9 @@ with st.sidebar:
     st.write("Contactémonos")
     st.write("jossue.cueto@gmail.com")  # Asegúrate de reemplazar esto con tu información real
     st.write("www.linkedin.com/in/jossue-cueto/")  # Asegúrate de reemplazar esto con tu información real
+    
+    response_cv = requests.get('https://raw.github.com/JossueCueto/portfolio_finance/c9be76bf1429f6b81d541ff8316c21a5b0e9e186/PDF/CV_Jossue.pdf')
+    pdf_cv = BytesIO(response.content)
     st.download_button('Descargar CV', 'https://raw.github.com/JossueCueto/portfolio_finance/c9be76bf1429f6b81d541ff8316c21a5b0e9e186/PDF/CV_Jossue.pdf', 'Jossue_CV.pdf')
 
 # Contenido principal de la aplicación que cambia basado en la selección del menú
