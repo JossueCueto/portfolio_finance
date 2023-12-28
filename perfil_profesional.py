8ee30d6eeb58b88aafa6888cff5b1a08d3cf5c52
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-28 09:02:26 -0500

    Update perfil_profesional.py

--- perfil_profesional.py
+++ perfil_profesional.py
@@ -14,11 +14,11 @@ def run_perfil_profesional():
   with col1:
       # Contenido columna 1
       st.markdown("<div style='text-align: center;'><strong>Certificado Bachiller</strong></div>", unsafe_allow_html=True)
-      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/blob/ea8c41b65549d641815ff14cdf5bd949aca7402e/PDF/Constancia_Orden_Merito_Jossue.pdf', 'Constancia_Orden_Merito_Jossue.pdf',key='bachiller')
+      st.download_button('Descargar', 'https://github.com/JossueCueto/portfolio_finance/eb31d4028ec06584205804a8d770434139c238ee/PDF/Certificado_bachiller.pdf', 'Jossue_Certificado_Bachiller.pdf',key='bachiller')
   with col2:
       # Contenido columna 2
       st.markdown("<div style='text-align: center;'><strong>Órden de Mérito</strong></div>", unsafe_allow_html=True)
-      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/blob/ea8c41b65549d641815ff14cdf5bd949aca7402e/PDF/Constancia_Orden_Merito_Jossue.pdf', 'Constancia_Orden_Merito_Jossue.pdf',key='orden')
+      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/blob/ea8c41b65549d641815ff14cdf5bd949aca7402e/PDF/Constancia_Orden_Merito_Jossue.pdf', 'Jossue_Constancia_Orden_Merito.pdf',key='orden')
   with col3:
       st.write("")
     
@@ -30,7 +30,7 @@ def run_perfil_profesional():
   with col1:
       # Contenido columna 1
       st.markdown("<div style='text-align: center;'><strong>Certificado</strong></div>", unsafe_allow_html=True)
-      st.download_button('Download Resume', 'path_to_resume.pdf', 'Tu Resumen.pdf',key='ofimatica')
+      st.download_button('Descargar', 'https://github.com/JossueCueto/portfolio_finance/eb31d4028ec06584205804a8d770434139c238ee/PDF/Certificado_itel.pdf', 'Jossue_Certificado_Itel',key='ofimatica')
   with col2:
       st.write("")
   with col3:
@@ -50,19 +50,19 @@ def run_perfil_profesional():
   with col1:
       # Contenido columna 1
       st.markdown("<div style='text-align: center;'><strong>Renta Fija</strong></div>", unsafe_allow_html=True)
-      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/blob/383cc0549d57c35fb255a61b257570f8194c9ee1/PDF/Fixed_income.pdf', 'Jossue_Fixed_Income.pdf',key='proyecto_1')
+      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/383cc0549d57c35fb255a61b257570f8194c9ee1/PDF/Fixed_income.pdf', 'Jossue_Fixed_Income.pdf',key='proyecto_1')
   with col2:
       # Contenido columna 2
       st.markdown("<div style='text-align: center;'><strong>Riesgo de Mercado</strong></div>", unsafe_allow_html=True)
-      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/blob/383cc0549d57c35fb255a61b257570f8194c9ee1/PDF/Market_Risk.pdf', 'Jossue_Market_Risk.pdf',key='proyecto_2')
+      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/383cc0549d57c35fb255a61b257570f8194c9ee1/PDF/Market_Risk.pdf', 'Jossue_Market_Risk.pdf',key='proyecto_2')
   with col3:
       # Contenido columna 3
       st.markdown("<div style='text-align: center;'><strong>Riesgo de Crédito</strong></div>", unsafe_allow_html=True)
-      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/blob/383cc0549d57c35fb255a61b257570f8194c9ee1/PDF/Credit_Risk.pdf', 'Jossue_Credit_Risk.pdf',key='proyecto_3')
+      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/383cc0549d57c35fb255a61b257570f8194c9ee1/PDF/Credit_Risk.pdf', 'Jossue_Credit_Risk.pdf',key='proyecto_3')
   with col4:
       # Contenido columna 4
       st.markdown("<div style='text-align: center;'><strong>Fincorp</strong></div>", unsafe_allow_html=True)
-      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/blob/383cc0549d57c35fb255a61b257570f8194c9ee1/PDF/Fincorp.pdf', 'Jossue_Fincorp.pdf',key='proyecto_4')
+      st.download_button('Descargar', 'https://raw.github.com/JossueCueto/portfolio_finance/383cc0549d57c35fb255a61b257570f8194c9ee1/PDF/Fincorp.pdf', 'Jossue_Fincorp.pdf',key='proyecto_4')
   
   st.header('Objetivos Profesionales para el 2024')
   st.subheader('CEFA - BCRP')
