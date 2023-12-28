9e3c5a98f2265d1bdb8895a3884858f6e7c42715
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-27 19:01:07 -0500

    Update perfil_profesional.py

--- perfil_profesional.py
+++ perfil_profesional.py
@@ -10,7 +10,7 @@ def run_perfil_profesional():
   st.write('Bachiller en Ciencias Administrativas, segundo lugar de mi promoción.')
   st.markdown(""" **Cursos relevantes:** Teoría Económica I y II, Formulación y evaluación de proyectos, Finanzas I, II y III""")
   # Crear 4 columnas del mismo tamaño
-  col1, col2,col3= st.columns(1,1,2)
+  col1, col2,col3= st.columns([1,1,2])
   with col1:
       # Contenido para la columna 1
       st.markdown("<div style='text-align: center;'><strong>Certificado Bachiller</strong></div>", unsafe_allow_html=True)
@@ -26,8 +26,7 @@ def run_perfil_profesional():
     
   with col3:
       st.write("")
-
-
+    
   st.subheader('Ofimática - ITEL')
   st.write('Técnico en Ofimática  ')
   st.markdown(""" **Cursos Relevantes:** Base de datos I y II, Hoja electrónica I y II (Excel y VBA)""")
