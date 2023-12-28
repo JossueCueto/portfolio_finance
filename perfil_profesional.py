a66e9384b34ce3b7f99806630a1c8680774b9217
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2023-12-27 19:44:46 -0500

    Update perfil_profesional.py

--- perfil_profesional.py
+++ perfil_profesional.py
@@ -89,14 +89,14 @@ def run_perfil_profesional():
              'representando un paso significativo hacia el reconocimiento profesional y la expansión de mis ' +
              'competencias en el sector financiero global.')
   st.header('Experiencia adicional')
-  st.subheader('Auxiliar Administrativo - SAEC EIRL (2022-2023)')
+  st.subheader('Auxiliar Administrativo - SAEC EIRL (2021-2022)')
   st.markdown("""
   Apoyo administrativo en la ejecución y mejora de los planes y proyectos de la empresa.
   - Ejecución y mejora de los procesos administrativos relacionados a gestión de activos fijos, prestación del servicio contable, gestión de RRHH y gestión de las redes sociales.
   - Apoyo en trámites relacionados a constitución de empresas, aumento de capital y otras modificaciones de estatutos.
   """)
   st.write('**Logro:** Mejora del sistema de gestión de activos fijo e implementación del SSST')
-  st.subheader('Asistente Administrativo - Secoemp EIRL (2021-2022)')
+  st.subheader('Asistente Administrativo - Secoemp EIRL (2020-2021)')
   st.markdown("""
   Apoyo administrativo en la ejecución de los planes y proyectos de la empresa.
   - Ejecución y mejora de los procesos administrativos relacionados a gestión de activos fijos, prestación del servicio contable, gestión de RRHH y gestión de las redes sociales.
