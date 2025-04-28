911b499a56807360103edaea4a4a243eb2b63f9a
Jossue Cueto <139831017+JossueCueto@users.noreply.github.com>
2025-04-28 15:40:54 -0500

    Update risk_free.py
    Elimina la línea yf.pdr_override()

--- risk_free.py
+++ risk_free.py
@@ -3,7 +3,6 @@ import pandas_datareader.data as pdr
 import yfinance as yf
 
 def risk_free_function(start=dt.date.today() - dt.timedelta(days=30),end=dt.date.today()):
-    yf.pdr_override()
     risk_free_data=pdr.get_data_yahoo('^TNX',start,end)['Adj Close']
     risk_free_rate = float(risk_free_data.iloc[-1])
     risk_free_rate=risk_free_rate/100
